794678ad0bc697354056000158f3dfefddd76436
charlie180-code <badersaadousalissou@gmail.com>
2025-03-14 13:43:23 +0100

    added UserMixin

--- apps/models/user.py
+++ apps/models/user.py
@@ -1,27 +1,24 @@
+from flask_login import UserMixin
 from .. import db
 from datetime import datetime
 
-
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     phone_number = db.Column(db.String)
-    email = db.Column(db.String)
-    password_hash = db.Column(db.String)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
     gender = db.Column(db.String)
     date_of_birth = db.Column(db.Date, default=datetime.utcnow)
     member_since = db.Column(db.Date, default=datetime.utcnow)
     transactions = db.relationship('Transaction', backref='user', lazy=True)
     wallets = db.relationship('Wallet', backref='user', lazy=True)
     active = db.Column(db.Boolean, default=True)
-    
-    @property
-    def is_active(self):
-        return self.active
+
+    def get_id(self):
+        return str(self.id)
 
     def __repr__(self):
         return f"<User {self.email}>"
-    
